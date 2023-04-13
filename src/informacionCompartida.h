d8c47f0bf6a7980c0bffc81be80ceaba100d21e2
Mario Araya <marioaraya1999@outlook.com>
2023-04-12 18:29:37 -0600

    informacionCompartida.h creado

---
+++ src/informacionCompartida.h
@@ -0,0 +1,17 @@
+struct informacionCompartida {
+    int emisores_vivos;
+    int emisores_creados;
+
+    int receptores_vivos;
+    int receptores_creados;
+
+    int celdas_buffer;
+
+    int contador_emisores;
+    int contador_receptores;
+
+	int tamano_archivo;
+	int tamano_info_compartida;
+    int llave;
+
+};
--- src/inicializador.c
+++ src/inicializador.c
@@ -7,25 +7,8 @@
 #include <semaphore.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include "informacionCompartida.h"
 
-
-struct informacionCompartida {
-    int emisores_vivos;
-    int emisores_creados;
-
-    int receptores_vivos;
-    int receptores_creados;
-
-    int celdas_buffer;
-
-    int contador_emisores;
-    int contador_receptores;
-
-	int tamano_archivo;
-	int tamano_info_compartida;
-    int llave;
-
-};
 
 void inicializarInformacionCompartida (struct informacionCompartida* informacion_compartida);
 
